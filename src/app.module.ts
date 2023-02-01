1f6e4cad75d55b41c967712c5f30814c97a5bd0c
abdjahiduddin <abd.jahiduddin@gmail.com>
2023-02-01 14:55:57 +0700

    Create connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config/dist';
+import { configValidationSchema } from './config/validation.schema';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigService } from '@nestjs/config/dist/config.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${process.env.STAGE}.stage.env`],
+      isGlobal: true,
+      validationSchema: configValidationSchema,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadModels: true,
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('HOST_PORT');
+  const env = process.env.STAGE.toUpperCase();
+
+  await app.listen(port);
+  logger.log(`[${env}] Application listening on port ${port}`);
 }
 bootstrap();
